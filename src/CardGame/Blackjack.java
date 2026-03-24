6237c229abdcb54e5bab54347458413e167abffc
ben9172 <benalarid306@gmail.com>
2026-03-24 20:54:34 +0000

    in progress 2

--- src/CardGame/Blackjack.java
+++ src/CardGame/Blackjack.java
@@ -3,15 +3,7 @@ package CardGame;
 public class Blackjack {
     public static void main(String[] args) {
         // Create a final int so that the maximum amount of cards cannot be changed
-        // final int maxDeck = 10;
-        // // Create the dealer's hand
-        // Card[] dealerHand = new Card[10];
-        // Card[] player = new Card[10];
-
-        // DeckOfCards deck = new DeckOfCards(); // 52 cards
-        // deck.shuffle();
-
-        // Create a final int so that the maximum amount of cards cannot be changed
+        final int MAX_CARDS = 11;
         DeckOfCards myDeckOfCards = new DeckOfCards();
         myDeckOfCards.shuffle(); // put Card objects in random order
 
@@ -33,19 +25,25 @@ public class Blackjack {
         System.out.println("You have: ");
         System.out.println(player);
 
-        for (int i = 0; i < 2; i += 1) {
+        while (true) {
 
             String playerChoice = Tools.MyTools.readString("Do you want to hit or stay (answer with hit or stay): ");
 
             if (playerChoice.equals("stay")) {
                 break;
             }
-            if (playerChoice.equals(("hit"))) {
-                String tempCard;
-                tempCard += player.addCard(myDeckOfCards.dealCard());
-                System.out.println("new hand after card added: " + tempCard);
+            if (playerChoice.equals("hit")) {
+                player.addCard(myDeckOfCards.dealCard());
+                System.out.println("New hand after card added: ");
+                System.out.println(player);
             }
+            System.out.println("Your total is: " + player.getHandValue());
         }
 
+        // dealer
+
+        while (dealer.getHandValue() < 17) {
+
+        }
     }
 }
--- src/CardGame/BlackjackHand.java
+++ src/CardGame/BlackjackHand.java
@@ -4,6 +4,7 @@ public class BlackjackHand {
 
     private Card[] hand;
     private int currentCard;
+    final int LOSE = 21;
 
     public BlackjackHand() {
         int currentCard = 0;
@@ -26,6 +27,48 @@ public class BlackjackHand {
             s += hand[i] + "\n";
         }
         return s;
+    }
+
+    public int getHandValue() {
+        int value = 0;
+        for (int i = 0; i < currentCard; i += 1) {
+            if (hand[i].getFace().equalsIgnoreCase("two")) {
+                value += 2;
+            } else if (hand[i].getFace().equalsIgnoreCase("three")) {
+                value += 3;
+            } else if (hand[i].getFace().equalsIgnoreCase("four")) {
+                value += 4;
+            } else if (hand[i].getFace().equalsIgnoreCase("five")) {
+                value += 5;
+            } else if (hand[i].getFace().equalsIgnoreCase("six")) {
+                value += 6;
+            } else if (hand[i].getFace().equalsIgnoreCase("seven")) {
+                value += 7;
+            } else if (hand[i].getFace().equalsIgnoreCase("eight")) {
+                value += 8;
+            } else if (hand[i].getFace().equalsIgnoreCase("nine")) {
+                value += 9;
+            } else if (hand[i].getFace().equalsIgnoreCase("jack")) {
+                value += 10;
+            } else if (hand[i].getFace().equalsIgnoreCase("queen")) {
+                value += 10;
+            } else if (hand[i].getFace().equalsIgnoreCase("king")) {
+                value += 10;
+                // If Ace is less than 21, it becomes an 11
+            } else if ((hand[i].getFace().equalsIgnoreCase("ace") && value < LOSE)) {
+                value += 11;
+                if (value >= 21) {
+                    value -= 10;
+                    return value;
+                }
+                // If Ace is less more 21, it becomes a 1 (probably wont work because you
+                // already lost)
+            } else if ((hand[i].getFace().equalsIgnoreCase("ace") && value > LOSE)) {
+                value += 1;
+            }
+            return value;
+        }
+
     }
 
 }
